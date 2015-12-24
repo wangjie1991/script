0b5bd913f4d01b20a75c5097d3e83b124a66f575
wangjie1991 <mononogy@sina.cn>
2015-12-24 14:21:20 +0800

    m

--- python/rename.py
+++ python/rename.py
@@ -2,42 +2,26 @@
 import os
 import sys
 
-# def getFormatOut(curNum):
-    # return '%05d' % curNum
-
-def getFormatOut(curNum):
-    if 0 <= curNum < 10:
-        return '0000%d' % curNum                
-    elif 10 <= curNum < 100:
-        return '000%d' % curNum
-    elif 100 <= curNum < 1000:
-        return '00%d' % curNum
-    elif 1000 <= curNum < 10000:
-        return '0%s' % curNum
-    else:
-        return str(curNum)
-
 if __name__ == '__main__':
-    if len(sys.argv) != 5:
-        print 'python %s inFile.list outBAT setName postfix' % sys.argv[0]
+    if len(sys.argv) != 4:
+        print 'python %s input output suffix' % sys.argv[0]
         sys.exit(1)
 
     fin = open(sys.argv[1],'r')
     fout = open(sys.argv[2],'w')
-    setName = sys.argv[3]
-    postfix = sys.argv[4]
-    tDic = {}
+    suffix = sys.argv[3]
+    dic = {}
 
     while 1:
         curLine = fin.readline()
         if not curLine: break
         baseName = curLine[curLine.rfind('/')+1:] 
-        tDic[baseName] = curLine.rstrip()
+        dic[baseName] = curLine.rstrip()
     fin.close()
 
     curNum = 0
-    for key in sorted(tDic.keys()):
-        fout.write('cp \'%s\' newName/%s_%s.%s\n' % (tDic[key],setName,getFormatOut(curNum),postfix))
+    for key in sorted(dic.keys()):
+        fout.write('cp \'%s\' newName/%05d.%s\n' % (dic[key], curNum, suffix))
         curNum += 1
     fout.close()
 
